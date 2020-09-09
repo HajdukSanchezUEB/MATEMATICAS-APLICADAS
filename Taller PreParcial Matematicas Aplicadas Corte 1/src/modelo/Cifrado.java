fe817c377746b410d43e15ac663be8baf0f7e2a8
jozek <jozek@DESKTOP-JHS>
2020-09-08 19:45:16 -0500

    Ejercicio 2 y 5 completos

--- Taller PreParcial Matematicas Aplicadas Corte 1/src/modelo/Cifrado.java
+++ Taller PreParcial Matematicas Aplicadas Corte 1/src/modelo/Cifrado.java
@@ -4,7 +4,7 @@ public class Cifrado {
 
 	private int x, z;
 	private String frase;
-	private char[] tablaCaracteres = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	private char[] tablaCaracteres = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
 	public int getX() {
 		return x;
@@ -38,7 +38,7 @@ public class Cifrado {
 		this.tablaCaracteres = tablaCaracteres;
 	}
 
-	public void encriptarFrase(int codigo, String frase) {
+	public void encriptarFrase(int x, String frase) {
 		StringBuilder cifrado = new StringBuilder();
 		int aux = 0, posicion;
 		int modulo = tablaCaracteres.length;
@@ -49,14 +49,32 @@ public class Cifrado {
 					break;
 				}
 			}
-			posicion = (aux + codigo) % modulo;
+			posicion = (aux + x) % modulo;
 			cifrado.append(tablaCaracteres[posicion]);
 		}
 		setFrase(cifrado.toString());
 	}
 
 	public void desencriptarFrase(int z, String frase) {
-		
+		StringBuilder cifrado = new StringBuilder();
+		int aux = 0, posicion = 0;
+		int modulo = tablaCaracteres.length;
+		for (int i = 0; i < frase.length(); i++) {
+			for (int j = 0; j < tablaCaracteres.length; j++) {
+				if (frase.charAt(i) == tablaCaracteres[j]) {
+					aux = j;
+					break;
+				}
+			}
+			int resta = aux - z;
+			if (resta < 0) {
+				posicion = modulo + resta;
+			} else {
+				posicion = (resta) % modulo;
+			}
+			cifrado.append(tablaCaracteres[posicion]);
+		}
+		setFrase(cifrado.toString());
 	}
 
 }
