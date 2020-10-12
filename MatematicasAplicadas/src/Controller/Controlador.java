5cf543a5279310f04d28f026ab1337f175af7674
jorge <jorge@192.168.1.69>
2020-10-12 10:39:39 -0500

    Commit

--- MatematicasAplicadas/src/Controller/Controlador.java
+++ MatematicasAplicadas/src/Controller/Controlador.java
@@ -14,12 +14,26 @@ public class Controlador {
 	private Vista vista;
 	public Controlador() {
 		vista = new Vista();
-		
+		individuos = new ArrayList<Individuos>();
 		iniciarProceso();
 	}
 
 	private void iniciarProceso() {
-
+		agregarIndividuos();
 	}
-
+	private void agregarIndividuos() {
+		int i = 1;	
+		boolean bandera = true;
+		while(bandera) {
+		int valorIndividuo = Integer.parseInt(vista.pedirDato("Ingrese el valor del indivuo " + i ));
+		int funcionAdaptacion = Integer.parseInt(vista.pedirDato("Ingrese la funcion de adaptacion indivuo " + i ));
+		Individuos individuo  = new Individuos(valorIndividuo,funcionAdaptacion);
+		individuos.add(individuo);
+		i ++;
+		if (vista.confirmarAccion("Desea agregar mas elementos?") == 1) {
+			bandera = false;
+		}
+		}
+	}
+	
 }
